fadb8fab7026d6a6190842b2c8a2fcd46f285312
NgCongLuan <luanbest177@gmail.com>
2023-11-16 13:43:31 +0700

    'abc'

---
+++ ShopDungCuTheThao/Areas/Admin/Controllers/QLBaiVietController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using ShopDungCuTheThao.Models;
+
+namespace ShopDungCuTheThao.Areas.Admin.Controllers
+{
+    public class QLBaiVietController : Controller
+    {
+        private ShopDungCuTheThaoDB db = new ShopDungCuTheThaoDB();
+        public ActionResult Index()
+        {
+            return View(db.baiViet);
+        }
+    }
+}
